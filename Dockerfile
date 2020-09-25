a23999de37b6c8fd5dff6bb51ac19ff5876ca20b
Anton Palgunov <toxblh@gmail.com>
2020-09-25 11:50:32 +0100

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:lts-alpine
+FROM node:current-alpine
 
 WORKDIR /code
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv'
 import TelegramBot from 'node-telegram-bot-api'
-import { connect } from 'skype-http'
+import { connect } from '@toxblh/skype-http'
 import { cleanSkypeMessage, getFileFromSkype } from './utils'
 
 dotenv.config()
